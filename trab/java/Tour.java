053e078a915a4baaf921d9e92e025212fa209fae
kenny <leonardongc@gmail.com>
2021-10-26 20:09:38 -0300

    pseudo código pronto (acho)

---
+++ trab/java/Tour.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+
+public class Tour {
+	public static void main(String[] args){
+	}
+}																																																																																																																										
