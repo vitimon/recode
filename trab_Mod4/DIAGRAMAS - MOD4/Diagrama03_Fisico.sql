d1137f13b0f58d2ca34c93b647c561c8cd2097cf
+kot <leonardongc@gmail.com>
2022-02-14 18:55:38 -0300

    atualização dos tipos no diagrama

--- trab_Mod4/DIAGRAMAS - MOD4/Diagrama03_Fisico.sql
+++ trab_Mod4/DIAGRAMAS - MOD4/Diagrama03_Fisico.sql
@@ -1,23 +1,23 @@
 /* Diagrama02_Logico: */
 
 CREATE TABLE Customer (
-    id_customer INTEGER PRIMARY KEY,
+    id_customer BIGINT PRIMARY KEY,
     email VARCHAR,
     name VARCHAR
 );
 
 CREATE TABLE Travel (
-    id_travel INTEGER PRIMARY KEY,
-    tickets TINYINT,
+    id_travel BIGINT PRIMARY KEY,
+    tickets SMALLINT,
     status VARCHAR,
+    date DATE,
     promoCode VARCHAR,
-    totalPrice MONEY,
-    fk_Customer_id_customer INTEGER,
-    fk_Transit_id_transit INTEGER
+    fk_Customer_id_customer BIGINT,
+    fk_Transit_id_transit BIGINT
 );
 
 CREATE TABLE Transit (
-    id_transit INTEGER PRIMARY KEY,
+    id_transit BIGINT PRIMARY KEY,
     origin VARCHAR,
     destination VARCHAR,
     price MONEY
