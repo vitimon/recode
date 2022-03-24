db411d1ff585ec9a23c5e6cd7226b6dd9a63f1c9
+kot <leonardongc@gmail.com>
2022-03-24 20:20:59 -0300

    montando componentes de exibição

--- trab_Mod5/recodeviagens-ui/src/components/Contents.jsx
+++ trab_Mod5/recodeviagens-ui/src/components/Contents.jsx
@@ -1,8 +1,8 @@
 
 import React from "react";
 
-export default function Contents() {
+export default function Contents(props) {
     return (
-       axios.get(api.baseUrl)
+       axios.get(api.baseUrl + props.resource)
     );
 }
--- trab_Mod5/recodeviagens-ui/src/views/Adm.jsx
+++ trab_Mod5/recodeviagens-ui/src/views/Adm.jsx
@@ -4,8 +4,8 @@ export default function Home() {
     return (
         <main className="home">
             <h2 className="titulo">Adicione ou Altere Novas Informações Por Aqui</h2><br />
-            <div class = "menuAlt">
-                <button>CLIENTES</button>
+            <div class="menuAlt">
+                <button >CLIENTES</button>
                 <button>DESTINOS</button>
                 <button>VIAGENS</button>
             </div>
